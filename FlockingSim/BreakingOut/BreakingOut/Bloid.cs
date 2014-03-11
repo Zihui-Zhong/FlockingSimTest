eb3fbf4b44ab0ef425b20b9219f025469f22000b
Zigui <zihui_zhong@hotmail.com>
2014-03-10 23:52:36 -0400

    Rename
    Added mouse interaction, optimisation

---
+++ FlockingSim/BreakingOut/BreakingOut/Bloid.cs
@@ -0,0 +1,262 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace BreakingOut
+{
+    class Bloid
+    {
+        Vector2 position;
+        Vector2 motion;
+
+        Vector2 acceleration;
+        double cohesionRange;
+        double alignementRange;
+        double separationRange;
+        double avoidRange;
+        double fearRange;
+        float maxSpeed;
+        Texture2D texture;
+        float alignementWeight;
+        float separationWeight;
+        float cohesionWeight;
+        float avoidWeight;
+        float fearWeight;
+        Rectangle screenRegion;
+
+        bool alive;
+
+        public Bloid(float x, float y,float dx, float dy, Texture2D t, Rectangle c)
+        {
+
+            acceleration = new Vector2(0, 0);
+            texture = t;
+            screenRegion = c;
+            position = new Vector2(x,y);
+            motion = new Vector2(dx, dy);
+            alignementRange = 50*50;
+            separationRange = 13*13;
+            cohesionRange = 50*50;
+            avoidRange = 50*50;
+            fearRange = 5;
+            maxSpeed = 0.5f*0.5f;
+            alignementWeight = 0.01f * maxSpeed;
+            avoidWeight = 0.06f * maxSpeed;
+            separationWeight = 0.1f * maxSpeed;
+            cohesionWeight = 0.0001f * maxSpeed;
+            fearWeight = 0.1f * maxSpeed;
+            alive = true;
+
+        }
+
+
+        public void updateAcceleration(List<Bloid>[] neighbours, Obstacle[] o)
+        {
+
+
+            acceleration.X = 0;
+            acceleration.Y = 0;
+            Vector2 f = Flock(neighbours);
+            Vector2 i = Interract(o);
+            Vector2 a = AttactMouse();
+
+            acceleration += i;
+            acceleration += f;
+            acceleration += a;
+
+
+        }
+        
+        public void updateMotion(Obstacle[] o)
+        {
+            motion += acceleration;
+            foreach (Obstacle obs in o)
+            {
+                obs.Collide(this);
+            }
+            if (motion.X*motion.X+motion.Y*motion.Y > maxSpeed)
+            {
+                motion.Normalize();
+                motion.X *= maxSpeed;
+                motion.Y *= maxSpeed;
+            }
+ 
+        }
+
+        public void updatePosition()
+        {
+            position += motion;
+        }
+        public bool updatePosition(float x, float y){
+            position += motion;
+            warpIfNecessairy();
+            if (position.X >= x && position.X <= x+100&& position.Y >= y&& position.Y <= y+100)
+            {
+                return false;
+            }
+            return true;
+        }
+        public void warpIfNecessairy()
+        {
+
+            if (position.X < 5)
+                position.X = 5;
+            if (position.Y < 5)
+                position.Y =5;
+            if (position.X > screenRegion.Width-5)
+                position.X = screenRegion.Width-5;
+            if (position.Y > screenRegion.Height-5)
+                position.Y = screenRegion.Height-5;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            if (alive)
+            {
+                spriteBatch.Draw(texture, new Vector2(position.X - texture.Width / 2, position.Y - texture.Height / 2), Color.White);
+            }
+        }
+
+        public Vector2 AttactMouse()
+        {
+            Vector2 a= new Vector2(0,0);
+            if (Mouse.GetState().LeftButton == ButtonState.Pressed)
+            {
+                a = new Vector2(Mouse.GetState().X - position.X, Mouse.GetState().Y - position.Y);
+                a.Normalize();
+                a *= (float)0.002;
+            }
+            return a;
+
+        }
+        public Vector2 Interract(Obstacle[] o)
+        {
+            int count = 0;
+            Vector2 sum = new Vector2(0, 0);
+
+            foreach (Obstacle obs in o)
+            {
+                Vector2 z = obs.Avoid(this);
+
+                if (z.X != 0 && z.Y != 0)
+                {
+                    sum += z*avoidWeight;
+                    count++;
+                }
+                Vector2 d = obs.Fear(this);
+
+                if (d.X != 0 && d.Y != 0)
+                {
+                    sum += d*fearWeight;
+                    count++;
+                }
+            }
+
+
+            if (count > 0)
+            {
+                sum.X /= count;
+                sum.Y /= count;
+            }
+
+            return sum;
+        }
+
+        public Vector2 Flock(List<Bloid>[] neighbours)
+        {
+            Vector2 c = new Vector2(0, 0);
+            Vector2 a = new Vector2(0, 0);
+            Vector2 s = new Vector2(0, 0);
+
+            Vector2 sum = new Vector2(0, 0);
+            int cCount = 0;
+            int aCount = 0;
+            int sCount = 0;
+
+            foreach (List<Bloid> b in neighbours)
+            {
+                foreach (Bloid bloid in b)
+                {
+                    if (bloid.isAlive())
+                    {
+                        Vector2 d = bloid.getPosition() - position;
+                        float dis=(d).X * d.X + d.Y * d.Y;
+                        if (dis < cohesionRange)
+                        {
+                            c += d;
+                            cCount++;
+                        }
+                        if (dis < alignementRange)
+                        {
+                            a += bloid.getMotion();
+                            aCount++;
+                        }
+                        if (dis > 0 && dis < separationRange)
+                        {
+
+                            dis = (float)Math.Sqrt(dis);
+                            s.X -= d.X / dis;
+                            s.Y -= d.Y / dis;
+                            sCount++;
+                        }
+                    }
+                }
+            }
+            if (cCount > 0)
+            {
+                c /= cCount;
+            }
+            if (aCount > 0)
+            {
+                a /= aCount;
+            }
+            if (sCount > 0)
+                s /= sCount;
+            {
+            }
+            c.X *= cohesionWeight;
+            c.Y *= cohesionWeight;
+            a.X *= alignementWeight;
+            a.Y *= alignementWeight;
+            s.X *= separationWeight;
+            s.Y *= separationWeight;
+            sum += c += a += s;
+            return sum;
+        }
+        public Vector2 getPosition()
+        {
+            return position;
+        }
+
+        public Vector2 getMotion()
+        {
+            return motion;
+        }
+        public double getAvoidRange()
+        {
+            return avoidRange;
+        }
+        public double getCollisionRange()
+        {
+            return fearRange;
+        }
+        public void kill()
+        {
+            alive = false;
+        }
+        public bool isAlive()
+        {
+            return alive;
+        }
+        public void setMotion(Vector2 m)
+        {
+            motion = m;
+        }
+    }
+
+
+}
--- FlockingSim/BreakingOut/BreakingOut/Game1.cs
+++ FlockingSim/BreakingOut/BreakingOut/Game1.cs
@@ -12,7 +12,7 @@ using Microsoft.Xna.Framework.Media;
 namespace BreakingOut
 {
     /// <summary>
-    /// This is the main type for your game
+    //Prototype d'un simulation de type flocking
     /// </summary>
     public class Game1 : Microsoft.Xna.Framework.Game
     {
@@ -48,6 +48,8 @@ namespace BreakingOut
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            this.IsMouseVisible = true;
+
 
             base.Initialize();
         }
@@ -68,14 +70,14 @@ namespace BreakingOut
             Texture2D tempTexture = Content.Load<Texture2D>("ball");
             int count = 0;
             int a = 1400;
-            int rangee = 5;
-            int colones = 7;
-
+            int rangee = 7;
+            int colones = 6;
+            int border = 50;
 
             alive = a;
             List<Bloid> temp = new List<Bloid>();
             Random random = new Random();
-            ;
+            
             for (int j = 1; j < 6; j++)
             {
                 for (int i = 0; i < a/5; i++)
@@ -93,13 +95,16 @@ namespace BreakingOut
             obs[count] = new ControlledCube(tempTexture, new Vector2(0, 0));
             tempTexture = Content.Load<Texture2D>("CityBlock");
             count++;
-            for (int i = 0; i < rangee; i++)
+            
+            int r = (screenRectangle.Width - border/2 - tempTexture.Width);
+            int c = (screenRectangle.Height - border/2 - tempTexture.Height);
+            int ecartWidth = tempTexture.Width + (screenRectangle.Width - tempTexture.Width * colones - border)/(colones-1);
+            int ecartHeight = tempTexture.Height + (screenRectangle.Height - tempTexture.Height * rangee - border) / (rangee - 1);
+            for (int i = 0; i < colones; i++)
             {
-                for (int j = 0; j < colones; j++)
+                for (int j = 0; j < rangee; j++)
                 {
-
-
-                    obs[count] = new CityBlocks(tempTexture, new Vector2((screenRectangle.Width - tempTexture.Width) - i * (screenRectangle.Width - tempTexture.Width) / (rangee - 1), (screenRectangle.Height - tempTexture.Height) - j * (screenRectangle.Height - tempTexture.Height) / (colones- 1)));
+                    obs[count] = new CityBlocks(tempTexture, new Vector2(r-ecartWidth*i, c-ecartHeight*j));
 
                     count++;
                 }
@@ -111,6 +116,7 @@ namespace BreakingOut
         {
             foreach (Bloid bloid in l)
             {
+                bloid.warpIfNecessairy();
                 bloids[(int)bloid.getPosition().X / 50, (int)bloid.getPosition().Y / 50].Add(bloid);
             }
         }
@@ -265,166 +271,3 @@ namespace BreakingOut
         }
     }
 }
-/*
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Audio;
-using Microsoft.Xna.Framework.Content;
-using Microsoft.Xna.Framework.GamerServices;
-using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
-using Microsoft.Xna.Framework.Media;
-
-namespace BreakingOut
-{
-    /// <summary>
-    /// This is the main type for your game
-    /// </summary>
-    public class Game1 : Microsoft.Xna.Framework.Game
-    {
-        GraphicsDeviceManager graphics;
-        SpriteBatch spriteBatch;
-        Paddle paddle;
-        Rectangle screenRectangle;
-        Ball ball;
-        int bricksWide = 10;
-        int bricksHigh = 5;
-        Texture2D brickImage;
-        Brick[,] bricks;
-
-        public Game1()
-        {
-            graphics = new GraphicsDeviceManager(this);
-            Content.RootDirectory = "Content";
-            graphics.PreferredBackBufferWidth = 750;
-            graphics.PreferredBackBufferHeight = 600;
-            screenRectangle = new Rectangle(0, 0, graphics.PreferredBackBufferWidth, graphics.PreferredBackBufferHeight);
-        }
-
-        /// <summary>
-        /// Allows the game to perform any initialization it needs to before starting to run.
-        /// This is where it can query for any required services and load any non-graphic
-        /// related content.  Calling base.Initialize will enumerate through any components
-        /// and initialize them as well.
-        /// </summary>
-        protected override void Initialize()
-        {
-            // TODO: Add your initialization logic here
-
-            base.Initialize();
-        }
-
-        /// <summary>
-        /// LoadContent will be called once per game and is the place to load
-        /// all of your content.
-        /// </summary>
-        protected override void LoadContent()
-        {
-            // Create a new SpriteBatch, which can be used to draw textures.
-            spriteBatch = new SpriteBatch(GraphicsDevice);
-            Texture2D tempTexture = Content.Load<Texture2D>("paddle");
-            paddle = new Paddle(tempTexture, screenRectangle);
-            tempTexture = Content.Load<Texture2D>("ball");
-            ball = new Ball(tempTexture, screenRectangle);
-
-            brickImage = Content.Load<Texture2D>("brick");
-            StartGame();
-        }
-        private void StartGame()
-        {
-            paddle.SetInStartPosition();
-            ball.SetInStartPosition(paddle.GetBounds());
-            bricks = new Brick[bricksWide, bricksHigh];
-
-            for (int y = 0; y < bricksHigh; y++)
-            {
-                Color tint = Color.White;
-                switch (y)
-                {
-                    case 0:
-                        tint = Color.Blue;
-                        break;
-                    case 1:
-                        tint = Color.Red;
-                        break;
-                    case 2:
-                        tint = Color.Green;
-                        break;
-                    case 3:
-                        tint = Color.Yellow;
-                        break;
-                    case 4:
-                        tint = Color.Purple;
-                        break;
-                }
-                for (int x = 0; x < bricksWide; x++)
-                {
-                    bricks[x, y] = new Brick(
-                    brickImage,
-                    new Rectangle(
-                    x * brickImage.Width,
-                    y * brickImage.Height,
-                    brickImage.Width,
-                    brickImage.Height),
-                    tint);
-                }
-            }
-        }
-
-        /// <summary>
-        /// UnloadContent will be called once per game and is the place to unload
-        /// all content.
-        /// </summary>
-        protected override void UnloadContent()
-        {
-            // TODO: Unload any non ContentManager content here
-        }
-
-        /// <summary>
-        /// Allows the game to run logic such as updating the world,
-        /// checking for collisions, gathering input, and playing audio.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
-        protected override void Update(GameTime gameTime)
-        {
-            // Allows the game to exit
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
-                this.Exit();
-
-            paddle.Update();
-            ball.Update();
-            ball.PaddleCollision(paddle.GetBounds());
-
-            foreach (Brick brick in bricks)
-                brick.CheckCollision(ball);
-            if (ball.OffBottom())
-                StartGame();
-
-            base.Update(gameTime);
-        }
-
-        /// <summary>
-        /// This is called when the game should draw itself.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
-        protected override void Draw(GameTime gameTime)
-        {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
-
-            spriteBatch.Begin();
-            paddle.Draw(spriteBatch);
-            ball.Draw(spriteBatch);
-
-            foreach (Brick brick in bricks)
-                brick.Draw(spriteBatch);
-                
-            
-            spriteBatch.End();
-
-            base.Draw(gameTime);
-        }
-    }
-}
-*/
